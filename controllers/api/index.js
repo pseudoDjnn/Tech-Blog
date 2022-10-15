629a18d227419bf06567e57ac9e66a5c050f0a63
p5eudoDjnn <alexander.havers@gmail.com>
2022-10-14 20:01:43 -0400

    sent simple handshake to database

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+// const router = require("express").Router();
+
+// const userRoutes = require("./user-routes");
+// const postRoutes = require("./post-routes");
+// const commentRoutes = require("./comment-routes");
+
+// // API Routes
+// router.use("/users", userRoutes);
+// router.use("/posts", postRoutes);
+// router.get("/comments", commentRoutes);
+
+// module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+// IMPORTS
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+// Using Express and calling the PORT
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require("./controllers/"));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on ${PORT}`));
+});
