ca24f6d022fc1027d853fa1017948cab0e0cb190
p5eudoDjnn <alexander.havers@gmail.com>
2022-10-16 11:58:58 -0400

    added cookie for back-end to help find the user

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,39 @@
 const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+const Url = require("url");
 
-router.get("/", (req, res) => {
-  res.send("hello world");
+// router.get("/", (req, res) => {
+//   res.send("hello world");
+// });
+
+// GRAB ALL ROUTES FOR HOMEPAGE
+router.get("/", async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      attributes: ["id", "title", "contnet", "created_at"],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "username"],
+        },
+        {
+          model: Comment,
+          attributes: ["id", "comment"],
+        },
+      ],
+    });
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    const x = Url.parse(req._parsedOriginalUrl, true);
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn,
+      active_home: x.path === "/",
+      homeLtrue,
+    });
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,26 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+// COOK-KEY
+const sess = {
+  secret: "Secret cookie is secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 // MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
